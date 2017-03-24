f9ec99e2bb7c4860ff31bd89e8134f7ba83c7c31
Laurent Rochette <lrochette@gmail.com>
2017-03-24 12:26:17 -0700

    Delete file before creating; exclude .DS_Store

--- createExtensionPackage.sh
+++ createExtensionPackage.sh
@@ -3,11 +3,16 @@
 #
 # Script to create the zip package to uplaod the the Chrome store
 #
-zip FlowCmdSyntaxHL.zip \
+FILE="FlowCmdSyntaxHL.zip"
+if [ -f $FILE ]; then
+  rm -f $FILE
+fi
+zip -r  $FILE \
   codemirror/addon codemirror/lib codemirror/mode \
   icon128.png icon16.png icon48.png \
   jquery-ui-1.10.2.custom.min.css jquery-ui-1.10.2.custom.min.js \
   jquery.js \
   manifest.json \
   myscript.js \
-  wait.js
+  wait.js \
+  -x "*.DS_Store*"
